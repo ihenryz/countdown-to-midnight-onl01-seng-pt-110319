9ef0f115487d750d0b947526f3b230db3283fa8e
Henry Phan <h.phan06@gmail.com>
2019-11-11 05:22:34 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(number)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep
+  number = 10
+  sleep 5
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  puts "HAPPY NEW YEAR!"
+end
